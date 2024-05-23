1210c412cfa217aad9f03dc30847fd2c38c0c78e
Aravindh7101 <aravindh7101@gmail.com>
2024-05-23 14:03:26 +0530

    js code add

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// console.log(document.getElementById("guestName").innerText);
+const firstName =prompt("what is your first name?")
+const lastName =prompt("what is your last name ?")
+
+const guuestName =document.getElementById("guestName")
+
+guuestName.textContent =firstName +' '+ lastName
+
+console.log(guuestName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // alert('Hello')
+  // prompt('prompting')
+  // confirm('conforming')
+
+  // dom == ("document object model")
+
+  // const firstName =prompt("what is your first name")
+  // const lastName =prompt("what is your last name ")
+  // const age =prompt("what is your age")
+
+  // console.log(firstName , lastName ,age);
